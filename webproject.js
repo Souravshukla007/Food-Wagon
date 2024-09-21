fa8c81cc11ed112aba1b7379eedd7a3ce33d2e26
SouravShukla007 <155749758+Souravshukla007@users.noreply.github.com>
2024-09-21 11:18:15 +0530

    Add files via upload

---
+++ webproject.js
@@ -0,0 +1,18 @@
+const att= document.getElementById("att");
+const sub= document.getElementById("sub");
+const h2= document.getElementById("h2");
+const h3= document.getElementById("h3");
+let times;
+sub.onclick = function(){
+    times = att.value;
+    times = Number(times);
+    if(name = ""){
+    alert("please enter the username!");
+    }
+    else if(pass = ""){
+    alert("please enter the password!");
+    }
+    else {
+        alert("please enter the password");
+    }
+}
